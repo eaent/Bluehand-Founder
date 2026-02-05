fde84a7f089ab9b2440ca10be64d4a8df000bba9
쪼녁2 <jacob122405@gmail.com>
2026-02-05 11:42:16 +0900

    add marker, not yet

---
+++ app.py
@@ -0,0 +1,26 @@
+# 기능 구현
+import mysql.connector
+
+import streamlit as st
+import folium
+from folium.plugins import LocateControl
+from streamlit_folium import st_folium
+
+st.title("현재 사용자 위치")
+
+# 사용자 위치 지도 출력
+st.set_page_config(layout="centered")
+
+m = folium.Map(location=[37.4974, 126.9277], zoom_start=18) # 임의 위치
+folium.plugins.LocateControl(auto_start=True).add_to(m) #선택사항  True 변경지
+LocateControl(
+    auto_start=False,
+    flyTo=True,
+    position="topright"
+).add_to(m)
+
+st_folium(m, width=1000, height=600)
+
+m = folium.Map([41.97, 2.81])
+
+st.title("서울권 센터")
